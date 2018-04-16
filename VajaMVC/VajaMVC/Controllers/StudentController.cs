bdab360a323086b61ae2936f7f1400312a2e57b6
b23 <b23@B23-03>
2018-04-16 10:12:24 +0200

    Osnovni del.

---
+++ VajaMVC/VajaMVC/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VajaMVC.Models;
+
+namespace VajaMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var studentList = new List<Student>{
+                new Student() { StudentId = 1, StudentName = "Jožef", Age = 18 },
+                new Student() { StudentId = 2, StudentName = "John Bon Johvi", Age = 19 },
+                new Student() { StudentId = 3, StudentName = "Miharocket", Age = 20 },
+                new Student() { StudentId = 4, StudentName = "Džek", Age = 16 },
+                new Student() { StudentId = 5, StudentName = "Kjpca", Age = 69 }
+            };
+            return View(studentList);
+        }
+    }
+}
