78df772f04ebfe0c99699f883a1e6cdf703ef78c
b23 <b23@B23-03>
2018-05-09 11:50:24 +0200

    Prodajalna program. (.cs/.json)

---
+++ Prodajalna/Prodajalna/Models/OrderContextInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Prodajalna.Models
+{
+    public class OrderContextInitializer:DropCreateDatabaseIfModelChanges<OrderContext>
+    {
+        protected override void Seed(OrderContext context)
+        {
+            var produkti = new List<Product>()
+            {
+                new Product(){Name="Paradižnikova juha", Price=1.3m,ActualCost=0.99m},
+                new Product(){Name="Šparuge", Price=6.9m,ActualCost=4.99m},
+                new Product(){Name="Lowski gulaš", Price=2.3m,ActualCost=0.69m}
+            };
+            foreach (var p in produkti)
+                context.Products.Add(p);
+            context.SaveChanges();
+
+            var naročilo = new Order() { Customer = "Bobo" };
+            var vrstice = new List<OrderDetail>()
+            {
+                new OrderDetail(){ Product=produkti[0], Quantity=2, Order=naročilo},
+                new OrderDetail(){ Product=produkti[1], Quantity=4, Order=naročilo},
+            };
+            context.Orders.Add(naročilo);
+            vrstice.ForEach(x => context.OrderDetails.Add(x));
+            context.SaveChanges();
+        }
+    }
+}
